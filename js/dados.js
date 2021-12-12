3cb93f10e6d08ecd610cf0214d02cb33c553baa6
Paulo Brandão <paulobrandaoofficial@gmail.com>
2021-12-12 16:20:08 -0300

    lógica de dados aplicada

---
+++ js/dados.js
@@ -0,0 +1,154 @@
+// EMPRESAS PARCEIRAS ---------------------------------------------------------------------------
+
+let empresas = [
+    {img : "avanade.png"},
+    {img : "cargox.png"},
+    {img : "darede.png"},
+    {img : "fyi.png"},
+    {img : "intelitrader.svg"},
+    {img : "linx.png"},
+    {img : "stil.svg"},
+    {img : "skillstelecom.png"},            
+];
+
+getEmpresas = () => {
+
+    let empresasParceiras   = document.querySelector('#empresasParceiras');
+    let listarempresas = empresas.map(item => "<div class='single-brand'><img src=img/empresas/"+item.img+" alt=''/></div>");           
+    empresasParceiras.innerHTML = listarempresas.join('');
+}
+
+getEmpresas();
+
+// APRESENTAÇÕES - GRUPOS -----------------------------------------------------------------------
+
+let apresentacoes = [
+    {
+        data : '16/12',
+        idData : 'dia1',
+        empresas : [
+            {
+                nome : 'Avanade Learning',
+                empresaImg : 'img/empresas/avanade.png',
+                grupoImg : 'img/empresas/avanade.png',
+                grupoQrCode: 'img/qrcode.png',
+                descricao: 'A Avanade Learning é uma plataforma de ensino que conta com cursos livres de diferentes áreas obtendo foco no ensino gamificado.',
+                time : 'Time 08',
+                horario : '9:00',
+                integrantes : [
+                    {nome : 'Pedro Henrique'},
+                    {nome : 'Gustavo Tolentino'},
+                    {nome : 'Joyce Santos'},
+                    {nome : 'Lucas Araújo'},
+                ],
+                mentores : 'Fernando Guerra'
+            },
+            {
+                nome : 'Avanade Learning',
+                empresaImg : 'img/empresas/avanade.png',
+                grupoImg : 'img/empresas/avanade.png',
+                grupoQrCode: 'img/qrcode.png',
+                descricao: 'A Avanade Learning é uma plataforma de ensino que conta com cursos livres de diferentes áreas obtendo foco no ensino gamificado.',
+                time : 'Time 08',
+                horario : '9:00',
+                integrantes : [
+                    {nome : 'Pedro Henrique'},
+                    {nome : 'Gustavo Tolentino'},
+                    {nome : 'Joyce Santos'},
+                    {nome : 'Lucas Araújo'},
+                ],
+                mentores : 'Fernando Guerra'
+            },
+        ]
+    },
+    {
+        data : '17/12',
+        idData : 'dia2',
+        empresas : [
+            {
+                nome : 'Avanade Learning 2',
+                empresaImg : 'img/empresas/avanade.png',
+                grupoImg : 'img/empresas/avanade.png',
+                grupoQrCode: 'img/qrcode.png',
+                descricao: 'A Avanade Learning é uma plataforma de ensino que conta com cursos livres de diferentes áreas obtendo foco no ensino gamificado.',
+                time : 'Time 08',
+                horario : '9:00',
+                integrantes : [
+                    {nome : 'Pedro Henrique 2'},
+                    {nome : 'Gustavo Tolentino 2'},
+                    {nome : 'Joyce Santos 2'},
+                    {nome : 'Lucas Araújo 2'},
+                ],
+                mentores : 'Fernando Guerra'
+            },
+        ]
+    },
+    
+]
+
+
+LoadData = () => {
+
+    let dias = '';
+    let timeline = '';
+    let empresasListadas = '';
+
+    apresentacoes.map((item, key) => {
+        
+        empresasListadas = '';
+
+        let active = '';
+        let show = '';
+        if(key == 0){ 
+            active = 'active';
+            show = 'show'
+        }
+
+        dias += 
+        `<li role="presentation">
+            <a class="${active}" href="#${item.idData}" aria-controls="${item.idData}" role="tab" data-bs-toggle="tab">${item.data}</a>
+        </li>`;
+        
+        item.empresas.map(empresa => {
+            empresasListadas += 
+            `<div class="single-timeline shadow-box">
+                <div class="timeline-left">
+                    <h4>${empresa.horario}</h4>
+                    <p>${empresa.time}</p>
+                    <img src="${empresa.empresaImg}" alt="">
+                </div>
+                <div class="timeline-right">
+                    <div class="timeline-img">
+                        <img src="${empresa.grupoImg}" alt="">
+                        <img src="${empresa.grupoQrCode}" alt="">
+                    </div>
+                    <div class="timeline-content">
+                        <h4>${empresa.nome}</h4>
+                        <p>${empresa.descricao}</p>
+                        <h5>Integrantes: </h5>
+                        <ol class="integrantes">
+                            `+ empresa.integrantes.map(integrante => `<li>${integrante.nome}</li>`).join('') +`
+                        </ol>
+                        <h5>Mentores: ${empresa.mentores}</h5>
+                    </div>
+                </div>
+            </div>`;
+        });
+
+        timeline += 
+        `<div role="tabpanel" class="tab-pane fade ${show} ${active}" id="${item.idData}">
+            <div class="total-timeline">
+                ${empresasListadas}
+            </div>
+        </div>
+        `;
+
+
+    });
+
+    document.querySelector('.timeline-tab').innerHTML = dias;
+    document.querySelector('.tab-content').innerHTML = timeline;
+}
+
+LoadData();
+
